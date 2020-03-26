3b6649ba1c32be4d88981baae44e0e324e9d97ba
yoh0811nkc <y-matsui@know-net.co.jp>
2020-03-26 16:47:32 +0900

    others

--- sw.js
+++ sw.js
@@ -1,53 +1,28 @@
-//キャッシュの設定
-var CACHE_NAME = 'Counter-cash';
-var urlsToCache = [
-  '/apps/Counter/app.html',
-  '/apps/Counter/app.css',
-  '/apps/Counter/app.js',
-];
+var CACHE_DYNAMIC_VERSION = 'dynamic-v1';
 
-//インストール
-self.addEventListener('install', function(event) {
-  event.waitUntil(caches.open(CACHE_NAME).then(function(cache){
-    console.log('Opened cache');
-    return cache.addAll(urlsToCache);
-  }));
-});
-
-//古いキャッシュの削除
-self.addEventListener('activate', function(event) {
-  var cacheWhitelist = [CACHE_NAME, ];
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(cacheNames.map(function(cacheName) {
-        console.log('delete old cache');
-        if (cacheWhitelist.indexOf(cacheName) === -1){
-          return caches.delete(cacheName);
-        }
-      }));
-    })
-  );
-});
-
-//キャッシュの利用
 self.addEventListener('fetch', function(event) {
-  console.log('fetch');
-  event.respondWith(caches.match(event.request).then(function(response){
-    // Cache hit - return response
-    if(response){
-      return response;
-    }
-    var fetchRequest = event.request.clone();
-    return fetch(fetchRequest).then(function(response){
-      // Check if we received a valid response
-      if(!response || response.status !== 200 || response.type !== 'basic') {
-        return response;
-      }
-      var responseToCache = response.clone();
-      caches.open(CACHE_NAME).then(function(cache) {
-        cache.put(event.request, responseToCache);
-      });
-      return response;
-    }));
-  }));
+  console.log('[Service Worker] Fetching something ...');
+  event.respondWith(
+    // キャッシュの存在チェック
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          // キャッシュがなければリクエストを投げて、レスポンスをキャッシュに入れる
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_VERSION)
+                .then(function(cache) {
+                  // 最後に res を返せるように、ここでは clone() する必要がある
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch(function() {
+              // エラーが発生しても何もしない
+            });
+        }
+      })
+  );
 });
