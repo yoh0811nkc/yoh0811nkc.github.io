d36d00922461d1f5dd025be5c0e022778cbbcbd3
yoh0811nkc <y-matsui@know-net.co.jp>
2020-03-26 16:05:02 +0900

    others

--- sw.js
+++ sw.js
@@ -1,76 +1,53 @@
-const CACHE_VERSION = 'v1';
-const CACHE_NAME = `${registration.scope}!${CACHE_VERSION}`;
-
-// キャッシュするファイルをセットする
-const urlsToCache = [
-  '.',
-  'common/jlogo.png',
-  'images/icons/icon-152x152.png',
-  'images/icons/icon-192x192.png',
-  'common.css'
+//キャッシュの設定
+var CACHE_NAME = 'Counter-cash';
+var urlsToCache = [
+  '/apps/Counter/app.html',
+  '/apps/Counter/app.css',
+  '/apps/Counter/app.js',
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    // キャッシュを開く
-    caches.open(CACHE_NAME)
-    .then((cache) => {
-      // 指定されたファイルをキャッシュに追加する
-      return cache.addAll(urlsToCache);
-    })
-  );
+//インストール
+self.addEventListener('install', function(event) {
+  event.waitUntil(caches.open(CACHE_NAME).then(function(cache){
+    console.log('Opened cache');
+    return cache.addAll(urlsToCache);
+  }));
 });
 
-self.addEventListener('activate', (event) => {
+//古いキャッシュの削除
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CACHE_NAME, ];
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return cacheNames.filter((cacheName) => {
-        // このスコープに所属していて且つCACHE_NAMEではないキャッシュを探す
-        return cacheName.startsWith(`${registration.scope}!`) &&
-               cacheName !== CACHE_NAME;
-      });
-    }).then((cachesToDelete) => {
-      return Promise.all(cachesToDelete.map((cacheName) => {
-        // いらないキャッシュを削除する
-        return caches.delete(cacheName);
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(cacheNames.map(function(cacheName) {
+        console.log('delete old cache');
+        if (cacheWhitelist.indexOf(cacheName) === -1){
+          return caches.delete(cacheName);
+        }
       }));
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-    .then((response) => {
-      // キャッシュ内に該当レスポンスがあれば、それを返す
-      if (response) {
+//キャッシュの利用
+self.addEventListener('fetch', function(event) {
+  console.log('fetch');
+  event.respondWith(caches.match(event.request).then(function(response){
+    // Cache hit - return response
+    if(response){
+      return response;
+    }
+    var fetchRequest = event.request.clone();
+    return fetch(fetchRequest).then(function(response){
+      // Check if we received a valid response
+      if(!response || response.status !== 200 || response.type !== 'basic') {
         return response;
       }
-
-      // 重要：リクエストを clone する。リクエストは Stream なので
-      // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
-      // 必要なので、リクエストは clone しないといけない
-      let fetchRequest = event.request.clone();
-
-      return fetch(fetchRequest)
-        .then((response) => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            // キャッシュする必要のないタイプのレスポンスならそのまま返す
-            return response;
-          }
-
-          // 重要：レスポンスを clone する。レスポンスは Stream で
-          // ブラウザ用とキャッシュ用の2回必要。なので clone して
-          // 2つの Stream があるようにする
-          let responseToCache = response.clone();
-
-          caches.open(CACHE_NAME)
-            .then((cache) => {
-              cache.put(event.request, responseToCache);
-            });
-
-          return response;
-        });
-    })
-  );
+      var responseToCache = response.clone();
+      caches.open(CACHE_NAME).then(function(cache) {
+        cache.put(event.request, responseToCache);
+      });
+      return response;
+    }));
+  }));
 });
